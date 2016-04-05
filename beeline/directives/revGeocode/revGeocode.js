0eeabbe0f6addad31dad034f7217ac87763d8aa8
daniel sim <daniel_sim@data.gov.sg>
2016-04-05 18:09:33 +0800

    Fix lost geocoding when re-entering suggestions page

--- beeline/directives/revGeocode/revGeocode.js
+++ beeline/directives/revGeocode/revGeocode.js
@@ -40,14 +40,7 @@ export default [
             var oneMapToken = await oneMapService.token();
             //var url = `http://staging.beeline.sg/onemap/revgeocode?location=${scope.x},${scope.y}`;
 
-            scope.geocodePromiseW = userService.beeline({
-                method: 'GET',
-                url: `/onemap/revgeocode?location=${scope.x},${scope.y}`
-            })
-            .then((response) => {
-                // console.log(response);
-                scope.geocodeW = response.data;
-
+            function updateDescription() {
                 if (scope.geocodeW && scope.geocodeW.GeocodeInfo[0].ROAD) {
                     scope.description1 = titleCase(scope.geocodeW.GeocodeInfo[0].BLOCK)
                         + ' ' + titleCase(scope.geocodeW.GeocodeInfo[0].ROAD);
@@ -55,11 +48,29 @@ export default [
                 }
                 else {
                     scope.description1 =
-                        parseFloat(scope.y).toFixed(4) +
+                        parseFloat(scope.y).toFixed(4) + ', ' +
                         parseFloat(scope.x).toFixed(4)
                     scope.description2 = null;
                 }
-            })
+            }
+            function geocode() {
+                scope.geocodePromiseW = userService.beeline({
+                    method: 'GET',
+                    url: `/onemap/revgeocode?location=${scope.x},${scope.y}`
+                })
+                .then((response) => {
+                    // console.log(response);
+                    scope.geocodeW = response.data;
+                    updateDescription();
+                })
+            }
+            scope.$watchGroup(['x', 'y'], geocode);
+            // Hack to reverse the erasure of the description when suggestion page is reloaded
+            scope.$watchGroup(['description1', 'description2'], function () {
+                if (scope.description1 == undefined || scope.description2 == undefined) {
+                    updateDescription();
+                }
+            });
         }, /* link(...) */
       };
     }
