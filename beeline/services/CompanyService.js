aa7ac47ed75bee87dcb2f4d397bd5e6bee807ceb
HONGYI LI <lhy@data.gov.sg>
2016-04-24 08:33:29 +0800

    cleaned up company cache

--- beeline/services/CompanyService.js
+++ beeline/services/CompanyService.js
@@ -1,28 +1,16 @@
-export default function CompanyService(UserService) {
-    var company;
-    return {
-        Company: function(id){
-            return $http.get("http://staging.beeline.sg/companies/"+id, {
-                headers: {
-                "Authorization": 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJyb2xlIjoidXNlciIsInVzZXJJZCI6MSwiaWF0IjoxNDU2Mzk2MTU4fQ.eCgMcdrhZAWfWcQ3hhcYts9oyQetZ4prGGf4t5xEAwU'
-                }
-        }).then(function(response){
-                return company = response.data;
-            });
-        },
-
-        getCompany(id){
-          return UserService.beeline({
-            url: `/companies/${id}`,
-            method: 'GET',
-          })
-          .then(function(response){
-            return response.data;
-          });
-        },
-
-        getcompany: function(){
-            return company;
-        },
-    };
-  }
+export function CompanyService(UserService) {
+  var companyCache = {};
+  return {
+    getCompany: function(id, ignoreCache) {
+      if (companyCache[id] && !ignoreCache) return companyCache[id];
+      return UserService.beeline({
+        url: '/companies/' + id,
+        method: 'GET',
+      })
+      .then(function(response) {
+        companyCache[id] = response.data;
+        return companyCache[id];
+      });
+    },
+  };
+}
