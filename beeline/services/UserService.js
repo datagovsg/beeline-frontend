30a49981dfad1f578958c112c24d6cb1a3a44148
HONGYI LI <lhy@data.gov.sg>
2016-04-27 03:52:44 +0800

    fixed prompts

--- beeline/services/UserService.js
+++ beeline/services/UserService.js
@@ -1,10 +1,104 @@
 import querystring from 'querystring';
 import uuid from 'uuid';
+import requestingVerificationCodeTemplate from '../templates/requesting-verification-code.html';
+import sendingVerificationCodeTemplate from '../templates/sending-verification-code.html';
 
-export default function UserService($http, $state, $ionicPopup, $rootScope) {
-  var preLoginState = null;
-  var preLoginParams = null;
+export default function UserService($http, $state, $ionicPopup, $ionicLoading) {
+
   var sessionToken = window.localStorage['sessionToken'] || null;
+
+  // Requests a verification code to log in
+  var sendTelephoneVerificationCode = function(number) {
+    return instance.beeline({
+      method: 'POST',
+      url: '/users/sendTelephoneVerification',
+      data: { 'telephone': '+65' + number },
+      headers: { 'Content-Type': 'application/json' }
+    }).then(function(response) {
+      return true;
+    });
+  };
+
+  // Submit the received code for verification
+  var verifyTelephone = function(telephoneNumber, code) {
+    return instance.beeline({
+      method: 'POST',
+      url: '/users/verifyTelephone?' + querystring.stringify({
+        telephone: '+65' + telephoneNumber,
+        code: code
+      })
+    })
+    .then(function(response) {
+      sessionToken = response.data.sessionToken;
+      window.localStorage.setItem('sessionToken', sessionToken);
+      instance.user = response.data.user;
+      window.localStorage.setItem('beelineUser', JSON.stringify(instance.user));
+      return instance.user;
+    });
+  };
+
+  // Prompts the user for a phone number to send a verification code
+  // Reprompts the user if the number given isnt a valid 8 digit string
+  // Returns a promise for the telephone number if successful
+  var VALID_PHONE_REGEX = /^[0-9]{8}$/; 
+  var promptForPhone = function(message) {
+    return $ionicPopup.prompt({
+      title: 'Add your phone number',
+      subTitle: message,
+      inputPlaceholder: 'e.g. 87654321'
+    })        
+    // Need to explicitly check for undefined to distinguish between empty string
+    // and an actual cancel
+    .then(function(response) { if (typeof response !== 'undefined') {
+      // If phone number is tested valid locally then transmit to server
+      if (VALID_PHONE_REGEX.test(response)) {
+        var telephone = response;
+        $ionicLoading.show({ template: requestingVerificationCodeTemplate });
+        return sendTelephoneVerificationCode(telephone)
+        .then(function() { 
+          $ionicLoading.hide();
+          return Promise.resolve(telephone); 
+        })
+        // If an error occurs close the loading dialogue before rethrowing it 
+        .catch(function(error) {
+          $ionicLoading.hide();
+          return Promise.reject(error);
+        });
+      }
+      // Reprompt with a message if the number given is invalid
+      else return promptForPhone("Please enter a valid 8 digit mobile number");
+    }});
+  };
+
+  // Prompt the user to enter the verification code sent to the given phone number
+  // Reprompts the user if the number given isnt a valid 6 digit string
+  var VALID_VERIFICATION_CODE_REGEX = /^[0-9]{6}$/; 
+  var promptForVerification = function(telephone, message) {
+    return $ionicPopup.prompt({
+      title: 'Verification Code',
+      subTitle: message,
+      inputPlaceholder: 'e.g. 123456'
+    })        
+    // Need to explicitly check for undefined to distinguish between empty string
+    // and an actual cancel
+    .then(function(response) { if (typeof response !== 'undefined') {
+      if (VALID_VERIFICATION_CODE_REGEX.test(response)) {
+        var verificationCode = response;
+        $ionicLoading.show({ template: sendingVerificationCodeTemplate });
+        return verifyTelephone(telephone, verificationCode)
+        .then(function() { 
+          $ionicLoading.hide();
+          return Promise.resolve(verificationCode); 
+        })
+        // If an error occurs close the loading dialogue before rethrowing it 
+        .catch(function(error) {
+          $ionicLoading.hide();
+          return Promise.reject(error);
+        });
+      }
+      else return promptForVerification(telephone, "Please enter a valid 6 digit code");       
+    }});
+  };
 
   var instance = {
     // If user data is set then user is logged in
@@ -37,36 +131,6 @@ export default function UserService($http, $state, $ionicPopup, $rootScope) {
         options.headers['Beeline-Device-Platform'] = 'Browser';
       }
       return $http(options);
-    },
-
-    // Requests a verification code to log in
-    sendTelephoneVerificationCode: function(number) {
-      return instance.beeline({
-        method: 'POST',
-        url: '/users/sendTelephoneVerification',
-        data: { 'telephone': '+65' + number },
-        headers: { 'Content-Type': 'application/json' }
-      }).then(function(response) {
-        return true;
-      });
-    },
-
-    // Submit the received code for verification
-    verifyTelephone: function(telephoneNumber, code) {
-      return instance.beeline({
-        method: 'GET', //TODO shouldnt this be post?
-        url: '/users/verifyTelephone?' + querystring.stringify({
-          telephone: '+65' + telephoneNumber,
-          code: code
-        })
-      })
-      .then(function(response) {
-        sessionToken = response.data.sessionToken;
-        window.localStorage.setItem('sessionToken', sessionToken);
-        instance.user = response.data.user;
-        window.localStorage.setItem('beelineUser', JSON.stringify(instance.user));
-        return instance.user;
-      });
     },
 
     // Prepares an update of the telephone number
@@ -136,67 +200,23 @@ export default function UserService($http, $state, $ionicPopup, $rootScope) {
       instance.user = null;
       delete window.localStorage['sessionToken'];
       delete window.localStorage['beelineUser'];
+      return Promise.resolve();
     },
 
     // Prompt the user for the phone number and login verification code
     logIn: function() {
-
-      // First prompt for the users phone number
-      var validPhone = /^[0-9]{8}$/; 
-      var promptForPhone = function(message) {
-        return $ionicPopup.prompt({
-          title: 'Add your phone number',
-          subTitle: message,
-          inputPlaceholder: 'e.g. 87654321'
-        })        
-        // Need to explicitly check for undefined to distinguish between empty string
-        // and an actual cancel
-        .then(function(response) { if (typeof response !== 'undefined') {
-          if (validPhone.test(response)) {
-            instance.logOut();
-            instance.sendTelephoneVerificationCode(response);
-            return Promise.resolve(response);
-          }
-          // Reprompt with a message if the number given is invalid
-          else return promptForPhone("Please enter a valid 8 digit mobile number");
-        }});
-      };
-
-      // Second prompt for verification code after getting a users phone number
-      var validCode = /^[0-9]{6}$/; 
-      var promptForVerification = function(telephone, message) {
-        return $ionicPopup.prompt({
-          title: 'Verification Code',
-          subTitle: message,
-          inputPlaceholder: 'e.g. 123456'
-        })        
-        // Need to explicitly check for undefined to distinguish between empty string
-        // and an actual cancel
-        .then(function(response) { if (typeof response !== 'undefined') {
-          if (validCode.test(response)) {
-            instance.verifyTelephone(telephone, response);
-            return Promise.resolve(response);
-          }
-          else return promptForVerification(telephone, "Please enter a valid 6 digit code sent to " + telephone);       
-        }});
-      };
-
-      promptForPhone("Please enter your mobile number to receive a verification code")
+      return promptForPhone("Please enter your mobile number to receive a verification code")
       .then(function(telephone) {
         promptForVerification(telephone, 'Enter the 6 digit code sent to ' + telephone);
+      })
+      .then(function() { return Promise.resolve() })
+      .catch(function(error) {
+        $ionicPopup.alert({
+          title: "Error when trying to connect to server",
+          subTitle: error
+        });
       });
-    },
-
-    // Return to the page that activated the login
-    afterLogin() {
-      $state.go(preLoginState || 'tabs.settings', preLoginParams);
-      preLoginState = undefined;
-    },
-
-    cancelLogin() {
-      $state.go(preLoginState, preLoginParams);
-      preLoginState = undefined;
-    },
+    }
 
   };
 
