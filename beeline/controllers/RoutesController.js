bdac6547740c057f2b6631ddd256945a655e1327
daniel sim <daniel_sim@data.gov.sg>
2016-06-10 16:54:57 +0800

    trigger resize map when switching between route list / map

--- beeline/controllers/RoutesController.js
+++ beeline/controllers/RoutesController.js
@@ -1,1 +1,8 @@
-export default function($scope) {}
+export default function($scope, uiGmapGoogleMapApi) {
+
+  $scope.$watch('activeTab', () => {
+    setTimeout(() => {
+      $scope.$broadcast('mapRequireResize')
+    }, 100)
+  })
+}
