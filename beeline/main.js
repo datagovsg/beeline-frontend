4fba37ccce63de8592b26adf210c64e2a4820e3b
daniel sim <daniel_sim@data.gov.sg>
2016-06-22 18:01:57 +0800

    remove references to old datepicker

--- beeline/main.js
+++ beeline/main.js
@@ -2,9 +2,6 @@ import {formatDate, formatDateMMMdd, formatTime,
         formatUTCDate, titleCase} from './shared/format';
 import {companyLogo} from './shared/imageSources';
 
-// Directive Imports
-import {DatePicker, TouchStart, TouchEnd, TouchMove, MouseMove} from './directives/datePicker/datePicker';
-
 global.moment = require('moment')
 
 // Configuration Imports
@@ -63,10 +60,6 @@ var app = angular.module('beeline', [
 .controller('TicketsController', require('./controllers/TicketsController.js').default)
 .controller('TicketDetailController', require('./controllers/TicketDetailController.js').default)
 .controller('BookingHistoryController', require('./controllers/BookingHistoryController.js').default)
-.directive('myTouchstart', TouchStart)
-.directive('myTouchend', TouchEnd)
-.directive('myTouchmove', TouchMove)
-.directive('myMousemove', MouseMove)
 .directive('suggestionViewer', require('./directives/suggestionViewer/suggestionViewer').default)
 .directive('startEndPicker', require('./directives/startEndPicker/startEndPicker').default)
 .directive('busStopSelector', require('./directives/busStopSelector/busStopSelector').default)
