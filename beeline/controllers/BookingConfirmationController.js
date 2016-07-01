6fe0d834356d4b28dd594e17de4bdd8c880cdbc1
daniel sim <daniel_sim@data.gov.sg>
2016-07-01 11:26:12 +0800

    Another back buttons implementation to avoid getting stuck

--- beeline/controllers/BookingConfirmationController.js
+++ beeline/controllers/BookingConfirmationController.js
@@ -5,9 +5,16 @@ export default [
   '$http',
   'BookingService',
   '$ionicHistory',
-  function($scope, $state, $http, BookingService, $ionicHistory) {
+  '$ionicPlatform',
+  function($scope, $state, $http, BookingService, $ionicHistory, $ionicPlatform) {
     $scope.$on('$ionicView.afterEnter', () => {
       $ionicHistory.clearHistory()
+
+      // Back button goes back to routes list
+      var deregister = $ionicPlatform.registerBackButtonAction(() => {
+        $state.go('tabs.routes');
+      }, 101)
+      $scope.$on('$ionicView.beforeLeave', deregister);
     });
   },
 ];
