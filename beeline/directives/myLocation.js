39fac3e1d4ad4fe3cd6df21e3f8be7df89323324
daniel sim <daniel_sim@data.gov.sg>
2016-07-15 15:31:43 +0800

    setup the zIndex of <my-location>, add to bus stop selector list

--- beeline/directives/myLocation.js
+++ beeline/directives/myLocation.js
@@ -3,15 +3,13 @@ export default function(uiGmapGoogleMapApi) {
 
   return {
     template: `
-<ui-gmap-circle ng-if="coords" idkey="idkey" center="coords" radius="radius"
+<ui-gmap-circle ng-if="coords" idkey="idkey1" center="coords" radius="radius"
   stroke="accuracyOptions.stroke" fill="accuracyOptions.fill"
   options="accuracyOptions.options"></ui-gmap-circle>
-<ui-gmap-marker ng-if="coords" idkey="idkey" coords="coords" options="markerOptions"></ui-gmap-marker>
+<ui-gmap-marker ng-if="coords" idkey="idkey2" coords="coords" options="markerOptions"></ui-gmap-marker>
     `,
 
-    scope: {
-      idkey: '=?',
-    },
+    scope: true,
     link(scope, elem, attr) {
       scope.coords = null; // Null until location is available
       scope.markerOptions = {
@@ -27,37 +25,36 @@ export default function(uiGmapGoogleMapApi) {
           color: '#3E82F7',
           opacity: 0.2,
         },
-        options: {
-          zIndex: 1
-        }
+        options: {}
       };
       scope.radius = 1;
-      scope.idkey = `my-location-${Date.now()}`
+      scope.idkey1 = `my-location-${Date.now()}-circle`
+      scope.idkey2 = `my-location-${Date.now()}-marker`
 
       uiGmapGoogleMapApi.then((googleMaps) => {
         scope.markerOptions.icon = {
           url: 'img/userLocation.svg',
           anchor: new googleMaps.Point(12,12),
         }
-      })
 
-      var watch = navigator.geolocation.watchPosition(
-        (success) => {
-          scope.coords = {
-            latitude: success.coords.latitude,
-            longitude: success.coords.longitude,
-          };
-          scope.radius = success.coords.accuracy;
-          scope.$digest(); // FIXME or apply?
-        }, (error) => {
-          scope.coords = null;
-          scope.$digest();
-        }, {
-          enableHighAccuracy: false,
-        });
+        var watch = navigator.geolocation.watchPosition(
+          (success) => {
+            scope.coords = {
+              latitude: success.coords.latitude,
+              longitude: success.coords.longitude,
+            };
+            scope.radius = success.coords.accuracy;
+            scope.$digest()
+          }, (error) => {
+            scope.coords = null;
+            scope.$digest()
+          }, {
+            enableHighAccuracy: false,
+          });
 
-      scope.$on('destroy', () => {
-        navigator.geolocation.clearWatch(watch);
+        scope.$on('destroy', () => {
+          navigator.geolocation.clearWatch(watch);
+        })
       })
     }
   }
