e4acae712b4de92ab0f306f006bb3d4c16c26477
shen_xujing <shen_xujing@ida.gov.sg>
2016-07-18 18:45:26 +0800

    change intro slide button to "DONE" for the last slide

--- beeline/controllers/IntroSlidesController.js
+++ beeline/controllers/IntroSlidesController.js
@@ -1,7 +1,24 @@
-export default['$scope', function($scope) {
+export default['$scope','$ionicHistory',function($scope, $ionicHistory) {
   $scope.options = {
     loop: false,
     effect: 'slide',
     speed: 500,
   };
-}];
+
+  $scope.data = {
+    slider: null,
+    button: 'SKIP'
+  }
+
+  $scope.$on("$ionicSlides.slideChangeEnd", function(event, data){
+    // note: the indexes are 0-based
+    $scope.activeIndex = data.slider.activeIndex;
+    if ($scope.activeIndex !== 2) {
+      $scope.data.button = 'SKIP'
+    }
+    if ($scope.activeIndex === 2) {
+      $scope.data.button = 'DONE'
+    }
+    $scope.$digest();
+  });
+}]
