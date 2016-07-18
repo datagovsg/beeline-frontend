13969fd15491b8e156f6d1611f166f7c1cb261e4
shen_xujing <shen_xujing@ida.gov.sg>
2016-07-18 19:07:48 +0800

    update button to buttonLabel

--- beeline/controllers/IntroSlidesController.js
+++ beeline/controllers/IntroSlidesController.js
@@ -7,17 +7,17 @@ export default['$scope',function($scope) {
 
   $scope.data = {
     slider: null,
-    button: 'SKIP'
+    buttonLabel: 'SKIP'
   }
 
   $scope.$on("$ionicSlides.slideChangeEnd", function(event, data){
     // note: the indexes are 0-based
     $scope.activeIndex = data.slider.activeIndex;
     if ($scope.activeIndex !== 2) {
-      $scope.data.button = 'SKIP'
+      $scope.data.buttonLabel = 'SKIP'
     }
     if ($scope.activeIndex === 2) {
-      $scope.data.button = 'DONE'
+      $scope.data.buttonLabel = 'DONE'
     }
     $scope.$digest();
   });
