4dc18665572ff5f45e19bec9488c65bfe9c7ea35
Yi Xin Ang <angyixin@gmail.com>
2016-07-19 14:52:04 +0800

    make the marker actor meet the gps plot

--- beeline/controllers/TicketDetailController.js
+++ beeline/controllers/TicketDetailController.js
@@ -105,30 +105,25 @@ export default [
       $scope.map.busLocation.icon = {
           url: 'img/busMarker.svg',
           scaledSize: new googleMaps.Size(68, 86),
+          anchor: new googleMaps.Point(34, 78),
         };
     })
 
     // Draw the icon for latest bus location
     $scope.$watch('recentPings', function(recentPings) {
       if (recentPings && recentPings.length > 0) {
-        $scope.map.busLocation.coordinates = recentPings[0].coordinates;
+        var busPosition = recentPings[0].coordinates.coordinates;
+        $scope.map.busLocation.coordinates = {
+          latitude: busPosition[1],
+          longitude: busPosition[0],
+        };
+
         $scope.map.lines.actualPath.path = recentPings.map(ping => ({
           latitude: ping.coordinates.coordinates[1],
-          longitude: ping.coordinates.coordinates[0]
+          longitude: ping.coordinates.coordinates[0],
         }));
       }
     });
-
-    //
-    $scope.$watch('map.markerOptions.boardMarker.icon', (icon) => {
-      if (!icon) return;
-      tripPromise.then((trip) => {
-        for (let ts of trip.tripStops) {
-          ts._markerOptions = ts.canBoard ? $scope.map.markerOptions.boardMarker :
-                                   $scope.map.markerOptions.alightMarker;
-        }
-      })
-    })
 
     // Pan and zoom to the bus location when the map is ready
     // Single ping request for updating the map initially
@@ -179,7 +174,6 @@ export default [
     Promise.all([mapPromise, uiGmapGoogleMapApi]).then(function(values) {
       var [map, googleMaps] = values;
 
-      MapOptions.disableMapLinks();
       $scope.$on("$ionicView.afterEnter", function(event, data) {
         googleMaps.event.trigger(map, 'resize');
       });
