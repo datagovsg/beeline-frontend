725ba1fd887887e25b65fa813c719b2abfd5eb29
Yi Xin Ang <angyixin@gmail.com>
2016-07-28 18:38:40 +0800

    fix the missing iOS done button for select tag

--- beeline/main.js
+++ beeline/main.js
@@ -104,7 +104,7 @@ var app = angular.module('beeline', [
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
     if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(false);
       cordova.plugins.Keyboard.disableScroll(true);
     }
     if (window.StatusBar) {
