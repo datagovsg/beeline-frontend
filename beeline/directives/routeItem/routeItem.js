f8b92a7724540c58123b5b845fec8320784bb3b6
Kaiwen <huangkaiw3n@gmail.com>
2016-08-17 18:52:40 +0800

    Modify routeItem directive to display transport company name and logo.
    *CompanyService used to get company name using route.transportCompanyId
    *Position of schedule on routeItem shifted below bus company name

--- beeline/directives/routeItem/routeItem.js
+++ beeline/directives/routeItem/routeItem.js
@@ -1,6 +1,6 @@
 import routeItemTemplate from './routeItem.html';
 
-export default function($state, BookingService) {
+export default function($state, BookingService, CompanyService) {
   return {
     replace: false,
     template: routeItemTemplate,
@@ -8,6 +8,19 @@ export default function($state, BookingService) {
       route: '=',
     },
     link: function(scope, element, attributes) {
+
+      scope.$watch('route', function() {
+        if (!scope.route.transportCompanyId) {
+          scope.companyName = '';
+          return;
+        }
+
+        var companyPromise = CompanyService.getCompany(scope.route.transportCompanyId)
+        .then((company) => {
+          scope.companyName = company.name;
+          return scope.companyName;
+        });
+      });
     },
   };
 }
