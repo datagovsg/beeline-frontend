bbc75458a9447ddc5f5cfc3d02162ae022adf1e7
Kaiwen <huangkaiw3n@gmail.com>
2016-08-23 16:19:58 +0800

    No longer pass route into routeItem directive

--- beeline/directives/routeItem/routeItem.js
+++ beeline/directives/routeItem/routeItem.js
@@ -5,7 +5,6 @@ export default function($state, BookingService) {
     replace: false,
     template: routeItemTemplate,
     scope: {
-      route: '=?',
       hideBusLabel: '<?',
       hideAdditionalInfo: '<?'
     },
