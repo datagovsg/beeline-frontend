f4bf133ad099e829f67570c463618c4f256e73b4
laptop PC <tuanzhang@gmail.com>
2016-10-12 15:33:01 +0800

    add recent backed kickstarter and days left before expiry

--- beeline/controllers/KickstarterController.js
+++ beeline/controllers/KickstarterController.js
@@ -1,5 +1,22 @@
 import _ from 'lodash';
 
+var transformKickstarter = function(kickstarter) {
+  for (let route of kickstarter) {
+    route.isValid = true;
+    if (route.notes && route.notes.lelongExpiry) {
+      var now = new Date().getTime();
+      var expiryTime = new Date(route.notes.lelongExpiry).getTime();
+      if (now >= expiryTime) {
+        route.isValid = false;
+      } else{
+        var day = 1000  * 60 * 60 * 24;
+        route.daysLeft =   Math.ceil((expiryTime - now)/day);
+      }
+    }
+  }
+  return kickstarter;
+}
+
 
 // Parse out the available regions from the routes
 // Filter what is displayed by the region filter
@@ -10,6 +27,7 @@ export default function($scope, $state, UserService, RoutesService, $q,
   // https://github.com/angular/angular.js/wiki/Understanding-Scopes
   $scope.data = {
     kickstarter: [],
+    backedKickstarter: []
   };
 
   $scope.refreshRoutes = function (ignoreCache) {
@@ -19,6 +37,10 @@ export default function($scope, $state, UserService, RoutesService, $q,
 
     // Configure the list of available regions
     kickstarterPromise.then(function(allRoutes) {
+      allRoutes = transformKickstarter(allRoutes).filter((route)=>{
+        return route.isValid;
+      })
+      console.log("ALLROUTE");
       console.log(allRoutes);
       $scope.data.kickstarter = _.sortBy(allRoutes, 'label');
       $scope.error = null;
@@ -48,10 +70,15 @@ export default function($scope, $state, UserService, RoutesService, $q,
                    alightStopId: bid.bid.tickets[0].alightStop.stopId,
                    bidPrice: bid.bid.userOptions.price}});
          $scope.recentBidsById = _.keyBy($scope.recentBids, r=>r.routeId);
-         console.log($scope.recentBids);
-         console.log($scope.recentBidsById);
-       }
+         var recentBidRouteIds = _.keys($scope.recentBidsById);
+         var recentAndAvailable = _.partition($scope.data.kickstarter, (x)=>{
+           return _.includes(recentBidRouteIds, x.id.toString());
+         });
+         //TODO: user delete bid from summary need to update this list
+         $scope.data.backedKickstarter = recentAndAvailable[0];
+         $scope.data.kickstarter = recentAndAvailable[1];
+      }
       firstRun = false;
-    })
+  })
 
 }
