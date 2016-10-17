c81f3daa4fdb2347408d9c93e6c8373c1054f503
shen_xujing <shen_xujing@ida.gov.sg>
2016-10-17 17:23:14 +0800

    progress bar directive to draw high and low bid info

---
+++ beeline/directives/progressBar/progressBar.js
@@ -0,0 +1,36 @@
+import progressBar from './progressBar.html';
+
+export default [
+  function() {
+    return {
+      restrict: 'E',
+      template: progressBar,
+      scope: {
+        curVal: '@',
+        maxVal: '@',
+        secondVal: '@'
+      },
+      link: function(scope, elem, attr) {
+
+        function updateProgress() {
+          var progress,secProgress = 0;
+
+          if (scope.maxVal) {
+            progress = Math.min(scope.curVal, scope.maxVal) / scope.maxVal * elem[0].querySelector('.fullBar').offsetWidth;
+            secProgress = Math.min(scope.secondVal, scope.maxVal) / scope.maxVal * elem[0].querySelector('.fullBar').offsetWidth;
+            console.log("BAR");
+            console.log(progress);
+            console.log(secProgress);
+          }
+
+          // elem[0].querySelector('.highBid').css('width', progress);
+          elem[0].querySelector('.highBid').style.width = progress+"px";
+          // elem[0].querySelector('.lowBid').css('width', secProgress);
+          elem[0].querySelector('.lowBid').style.width = secProgress+"px";
+        }
+
+        scope.$watchGroup(['curVal','secVal','maxVal'], updateProgress);
+      }
+    }
+  }
+]
--- beeline/main.js
+++ beeline/main.js
@@ -101,6 +101,7 @@ var app = angular.module('beeline', [
 .directive('mapBusStops', require('./directives/mapBusStops').default)
 .directive('dynamicSignage', require('./directives/dynamicSignage.js').default)
 .directive('beelineBindHtml', require('./directives/beelineBindHtml.js').default)
+.directive('progressBar', require('./directives/progressBar/progressBar.js').default)
 .config(configureRoutes)
 .config(function($ionicConfigProvider) {
   $ionicConfigProvider.tabs.position('bottom');
