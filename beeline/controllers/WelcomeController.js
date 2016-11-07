fcef2e6b15734bff67b4c71b8549ee94abf6b69e
Ray <chengwm@gmail.com>
2016-11-07 17:58:45 +0800

    Data retrieval for welcome page

--- beeline/controllers/WelcomeController.js
+++ beeline/controllers/WelcomeController.js
@@ -1,3 +1,22 @@
-export default['$scope',function($scope) {
+const queryString = require('querystring')
+
+export default['$scope', '$stateParams', 'UserService',
+async function($scope, $stateParams, UserService) {
+
+	$scope.refCode = $stateParams.refCode || null;
+	// $scope.refCode = $stateParams.refCode || throw Error("No referral code provided");
+
+	if($scope.refCode){
+		var query = queryString.stringify({code: $scope.refCode})
+
+		var result = await UserService.beeline({
+			method: 'GET',
+			url: '/promotions/getRefCodeOwner?'+query,
+		});
+
+		$scope.refCodeOwner = result.data || null;
+		// $scope.refCodeOwner = result.data || throw Error("Invalid referral code");
+	} 
+	// if refCode is null OR if refCodeOwner is null, throw error
 
 }]
