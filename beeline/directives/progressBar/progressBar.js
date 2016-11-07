f541a9e2cf95920505af786d4b9fdf04bdf0cb98
shen_xujing <shen_xujing@ida.gov.sg>
2016-11-07 23:25:57 +0800

    1 tier progress bar

--- beeline/directives/progressBar/progressBar.js
+++ beeline/directives/progressBar/progressBar.js
@@ -6,34 +6,10 @@ export default [
       restrict: 'E',
       template: progressBar,
       scope: {
-        curVal: '@',
-        maxVal: '@',
-        secondVal: '@',
-        highPrice: '@',
-        lowPrice: '@',
-        highTotal: '@'
+        backer1: '@',
+        pax1: '@',
+        price1: '@',
       },
-      link: function(scope, elem, attr) {
-
-        function updateProgress() {
-          var progress,secProgress = 0;
-
-          if (scope.maxVal) {
-            progress = Math.min(scope.curVal, scope.maxVal) / scope.maxVal * elem[0].querySelector('.fullBar').offsetWidth;
-            secProgress = Math.min(scope.secondVal, scope.maxVal) / scope.maxVal * elem[0].querySelector('.fullBar').offsetWidth;
-          }
-
-          // elem[0].querySelector('.highBid').css('width', progress);
-          elem[0].querySelector('.highBid').style.width = progress+"px";
-          // elem[0].querySelector('.lowBid').css('width', secProgress);
-          elem[0].querySelector('.lowBid').style.width = secProgress+"px";
-
-          scope.highNeeded = scope.highTotal - scope.curVal;
-          scope.lowNeeded = scope.maxVal - scope.secondVal;
-        }
-
-        scope.$watchGroup(['curVal','secVal','maxVal', 'highTotal'], updateProgress);
-      }
     }
   }
 ]
