793ab7efedc59e41f8a033b1331b42178ea8799a
shen_xujing <shen_xujing@ida.gov.sg>
2016-11-10 16:13:25 +0800

    more changes on progress-bar

--- beeline/directives/progressBar/progressBar.js
+++ beeline/directives/progressBar/progressBar.js
@@ -6,11 +6,20 @@ export default [
       restrict: 'E',
       template: progressBar,
       scope: {
-        backer1: '@',
-        pax1: '@',
-        price1: '@',
-        detail: '@',
+        backer1: '<',
+        pax1: '<',
+        price1: '<',
+        detail: '<',
       },
+      link: function(scope, elem, attr) {
+        scope.$watchGroup(['backer1', 'pax1'], () => {
+          scope.moreNeeded = Math.max((scope.pax1-scope.backer1), 0);
+
+          if (!isFinite(scope.moreNeeded))
+            scope.moreNeeded = '';
+          console.log(scope.moreNeeded)
+        })
+      }
     }
   }
 ]
