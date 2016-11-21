c31c255f6519901d77ffdb37555c4cf13c9fae76
shen_xujing <shen_xujing@ida.gov.sg>
2016-11-21 14:01:36 +0800

    in service window ends at the last stop time (no more buffer)

--- beeline/directives/inServiceWindow.js
+++ beeline/directives/inServiceWindow.js
@@ -31,9 +31,9 @@ export default function ($timeout) {
           var allStopsTime = _.flatten(trips.map(trip=>trip.tripStops))
                             .map(stop=>stop.time).sort();
           if (allStopsTime) {
-            //15 min before 1st stop time and 15 mins after last stop time
+            //15 min before 1st stop time
             scope.startTime = new Date(allStopsTime[0]).getTime() - 1000*60*15;
-            scope.endTime = new Date(allStopsTime[allStopsTime.length-1]).getTime() + 1000*60*15;
+            scope.endTime = new Date(allStopsTime[allStopsTime.length-1]).getTime();
             scope.now = Date.now();
             if (isFinite(scope.startTime) && isFinite(scope.endTime)) {
               scope.inServiceWindow = scope.startTime <= scope.now && scope.now <= scope.endTime;
