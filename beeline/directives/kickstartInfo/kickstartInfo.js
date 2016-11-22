70bfbdcdedac2c5ed09f5482cf29a0c0a132b85a
Yi Xin Ang <angyixin@gmail.com>
2016-11-22 16:36:48 +0800

    commenting kickstarter info directive

--- beeline/directives/kickstartInfo/kickstartInfo.js
+++ beeline/directives/kickstartInfo/kickstartInfo.js
@@ -1,8 +1,8 @@
-//this is found in kickstarter-detail, kickstarter-summary, kickstarter-recap, kickstarter-commit.
-//information will show and hide accordingly
-//passHide hides the pass in summary page
-//extraHide hides the extra info about the kickstarter route in multiple pages
-//preorderHide hides the info about "preordering" in recap and commit page
+// this is found in kickstarter-detail, kickstarter-summary, kickstarter-recap, kickstarter-commit.
+// information will show and hide accordingly
+// passHide hides the pass in summary page
+// extraHide hides the extra info about the kickstarter route in multiple pages
+// preorderHide hides the info about asking user to "preorder", takes effect in recap and commit page
 
 
 export default function () {
