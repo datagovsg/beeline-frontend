434a67f11854ad373d46abac4a4730321750be61
shen_xujing <shen_xujing@ida.gov.sg>
2016-11-29 11:34:35 +0800

    1. share getUniqueRegionsFromRoutes in RoutesService 2. make "timeoutPromise" for first load kickstart list

--- beeline/controllers/KickstarterController.js
+++ beeline/controllers/KickstarterController.js
@@ -2,21 +2,13 @@ import _ from 'lodash';
 import kickstartHelpTemplate from '../templates/kickstart-popup.html';
 import loadingTemplate from '../templates/loading.html';
 
-// Return an array of regions covered by a given array of routes
-function getUniqueRegionsFromRoutes(routes) {
-  return _(routes).map(function(route) {return route.regions;})
-  .flatten()
-  .uniqBy('id')
-  .sortBy('name')
-  .value();
-}
 
 // Parse out the available regions from the routes
 // Filter what is displayed by the region filter
 // Split the routes into those the user has recently booked and the rest
 export default function($scope, $state, UserService, RoutesService, $q,
   $ionicScrollDelegate, $ionicPopup, KickstarterService, $ionicLoading,
-  SearchService, $timeout) {
+  SearchService, $timeout, loadingSpinner) {
 
   // https://github.com/angular/angular.js/wiki/Understanding-Scopes
   $scope.data = {
@@ -41,33 +33,26 @@ export default function($scope, $state, UserService, RoutesService, $q,
     })
   }
 
-  //show loading spinner for the 1st time
-  var timeout;
-  $scope.$watch(()=>KickstarterService.getLelong(), (lelongRoutes)=>{
-    if (!lelongRoutes) {
-      $ionicLoading.show({
-        template: loadingTemplate
-      })
-      //1 min buffer if list cannot load
-      timeout = $timeout(()=>{
-        if (!lelongRoutes) {
-          $ionicLoading.hide();
-          $scope.data.error = true;
-        }
-      }, 1000*60)
-    } else {
-      if (timeout) {
-        $timeout.cancel(timeout);
-      }
-      $ionicLoading.hide();
-      $scope.data.error = null;
-      if (!window.localStorage['showKickstarter']) {
-        window.localStorage['showKickstarter'] = true;
-        $scope.showHelpPopup();
-      }
-    }
-  });
+  var timeoutProise = function(promise, ms) {
+    return Promise.race([promise, new Promise((resolve,reject)=>{
+      $timeout(()=>reject(), ms);
+    })])
+  }
 
+  //show loading spinner for the 1st time
+  loadingSpinner(timeoutProise(KickstarterService.fetchLelong(), 10*6000)
+                  .then(()=>{
+                    $scope.data.error = null;
+                  })
+                  .catch(()=>{
+                    $scope.data.error = true;
+                  })
+                  .then(()=>{
+                    if (!window.localStorage['showKickstarter']) {
+                      window.localStorage['showKickstarter'] = true;
+                      $scope.showHelpPopup();
+                    }
+                  }));
 
   $scope.$watchGroup([
     ()=>KickstarterService.getLelong(),
@@ -77,7 +62,6 @@ export default function($scope, $state, UserService, RoutesService, $q,
   ], ([lelongRoutes, userBids, selectedRegionId, filterText])=>{
       if (!lelongRoutes) return;
       $scope.data.kickstarter = _.sortBy(lelongRoutes, 'label');
-      $scope.data.regions = getUniqueRegionsFromRoutes($scope.data.kickstarter);
       $scope.userBids = userBids;
       $scope.recentBidsById = _.keyBy($scope.userBids, r=>r.routeId);
       var recentAndAvailable = _.partition($scope.data.kickstarter, (x)=>{
@@ -86,6 +70,8 @@ export default function($scope, $state, UserService, RoutesService, $q,
       $scope.data.backedKickstarter = recentAndAvailable[0];
       //don't display it in kickstarter if it's expired
       $scope.data.kickstarter = recentAndAvailable[1].filter((route)=>!route.isExpired);
+      //regions from list of backed and not expired available
+      $scope.data.regions = RoutesService.getUniqueRegionsFromRoutes($scope.data.backedKickstarter.concat($scope.data.kickstarter));
       $scope.data.filteredKickstarter = SearchService.filterRoutes($scope.data.kickstarter, +selectedRegionId, filterText);
       $scope.data.filteredbackedKickstarter = SearchService.filterRoutes($scope.data.backedKickstarter, +selectedRegionId, filterText);
 
