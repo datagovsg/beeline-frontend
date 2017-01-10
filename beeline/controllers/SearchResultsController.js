9e338c42d9204c73e425262060994e64b4d1a29a
daniel <daniel_sim@data.gov.sg>
2017-01-10 18:00:19 +0800

    1. implement lite route search and display 2. link to suggest page

--- beeline/controllers/SearchResultsController.js
+++ beeline/controllers/SearchResultsController.js
@@ -2,10 +2,11 @@ import _ from 'lodash';
 import assert from 'assert';
 import querystring from 'querystring';
 
-export default function($scope, $state, $stateParams, $http, UserService) {
+export default function($scope, $state, $stateParams, $http, UserService, LiteRoutesService) {
 
   $scope.runningRoutes = null;
   $scope.crowdstartRoutes = null;
+  $scope.liteRoutes = null;
 
   $scope.$watchCollection(() => [
     $stateParams.originLat,
@@ -15,7 +16,7 @@ export default function($scope, $state, $stateParams, $http, UserService) {
   ], ([slat, slng, elat, elng]) => {
     assert(slat && slng && elat && elng);
 
-    UserService.beeline({
+    const runningPromise = UserService.beeline({
       url: '/routes/search_by_latlon?' + querystring.stringify({
         startLat: slat,
         startLng: slng,
@@ -29,7 +30,7 @@ export default function($scope, $state, $stateParams, $http, UserService) {
       $scope.runningRoutes = result.data;
     })
 
-    UserService.beeline({
+    const lelongPromise = UserService.beeline({
       url: '/routes/search_by_latlon?' + querystring.stringify({
         startLat: slat,
         startLng: slng,
@@ -42,6 +43,41 @@ export default function($scope, $state, $stateParams, $http, UserService) {
     .then((result) => {
       $scope.crowdstartRoutes = result.data;
     })
+
+    const litePromise = UserService.beeline({
+      url: '/routes/search_by_latlon?' + querystring.stringify({
+        startLat: slat,
+        startLng: slng,
+        endLat: elat,
+        endLng: elng,
+        maxDistance: 500,
+        tags: JSON.stringify(['lite']),
+      })
+    })
+    .then((result) => {
+      $scope.liteRoutes = LiteRoutesService.transformLiteRouteData(result.data);
+    })
+
   })
+
+  $scope.$watchGroup(['liteRoutes', 'crowdstartRoutes', 'runningRoutes'], (routes) => {
+    $scope.routesFoundCount = _.sumBy(routes, r => r ? r.length : 0)
+  })
+
+  $scope.submitSuggestion = () => {
+    var href = "https://www.beeline.sg/suggest.html#" + querystring.stringify({
+      originLat: $stateParams.originLat,
+      originLng: $stateParams.originLng,
+      destinationLat: $stateParams.destinationLat,
+      destinationLng: $stateParams.destinationLng,
+    });
+
+    if (typeof cordova !== 'undefined') {
+      cordova.InAppBrowser.open(href, '_system');
+    }
+    else {
+      window.open(href, '_blank');
+    }
+  };
 
 };
