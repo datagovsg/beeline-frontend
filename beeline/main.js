a31b27d21588268ec883be586aee4bb33d79c8ab
shen_xujing <shen_xujing@ida.gov.sg>
2017-03-09 17:46:55 +0800

    allow scroll when soft keyboard comes up

--- beeline/main.js
+++ beeline/main.js
@@ -141,7 +141,7 @@ angular.module('beeline')
     // for form inputs)
     if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(false);
-      cordova.plugins.Keyboard.disableScroll(true);
+      cordova.plugins.Keyboard.disableScroll(false);
     }
     if (window.StatusBar) {
       // org.apache.cordova.statusbar required
