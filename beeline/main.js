f7c9cf997e80227b85c43b3541b1c5160e20286c
shen_xujing <shen_xujing@ida.gov.sg>
2017-03-13 11:42:01 +0800

    fix #289

--- beeline/main.js
+++ beeline/main.js
@@ -199,10 +199,17 @@ angular.module('beeline')
   $templateCache.put('templates/tab-booking-confirmation.html', require('../www/templates/tab-booking-confirmation.html'))
 })
 
-var devicePromise = window.cordova ?
-  new Promise((resolve) => {
+var devicePromise = new Promise((resolve, reject) => {
+  if (window.cordova) {
     document.addEventListener('deviceready', resolve, false);
-  }) : null;
+  }
+  else {
+    console.log('No cordova detected')
+    resolve();
+  }
+})
+
+app.service('DevicePromise', () => devicePromise);
 
 app.run(['UserService', '$ionicPopup', async function (UserService, $ionicPopup) {
   // Version check, if we're in an app
