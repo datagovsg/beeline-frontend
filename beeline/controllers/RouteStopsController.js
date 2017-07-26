629db0989027fcc12e737575129229d51b6c61f9
HONGYI LI <lhy@data.gov.sg>
2017-07-26 14:35:11 +0800

    Basic functionality for stoppicker

--- beeline/controllers/RouteStopsController.js
+++ beeline/controllers/RouteStopsController.js
@@ -3,11 +3,67 @@ export default [
   "$state",
   "$stateParams",
   "$ionicHistory",
-  function($scope, $state, $stateParams, $ionicHistory) {
-    $scope.routeId = $stateParams.routeId;
-    $scope.chooseStop = function(stop) {
-      $stateParams.callback(stop);
+  "$ionicLoading",
+  "$ionicPopup",
+  "$ionicScrollDelegate",
+  "RoutesService",
+  "BookingService",
+  function(
+    $scope, 
+    $state, 
+    $stateParams, 
+    $ionicHistory, 
+    $ionicLoading,
+    $ionicPopup,
+    $ionicScrollDelegate,
+    RoutesService, 
+    BookingService
+  ) {
+    // ------------------------------------------------------------------------
+    // Input
+    // ------------------------------------------------------------------------
+    let routeId = $stateParams.routeId ? +$stateParams.routeId : null;
+    let type = $stateParams.type;
+    let stop = $stateParams.stop ? +$stateParams.stop : null;
+    let callback = $stateParams.callback;
+    // ------------------------------------------------------------------------
+    // Model
+    // ------------------------------------------------------------------------
+    $scope.data = {
+      stops: null, // array of stop objects
+      selectedStopId: null // int
+    };
+    // ------------------------------------------------------------------------ 
+    // Hooks 
+    // ------------------------------------------------------------------------
+    $scope.selectStop = (stopId) => { $scope.data.selectedStopId = stopId; };
+    $scope.done = () => { 
+      if (typeof callback === "function") callback($scope.data.selectedStopId);
       $ionicHistory.goBack();
     };
+    // ------------------------------------------------------------------------
+    // Initialization
+    // ------------------------------------------------------------------------
+    $scope.data.selectedStopId = stop;
+    $ionicLoading.show({
+      template: `<ion-spinner icon='crescent'></ion-spinner><br/><small>Loading stop information</small>`,
+      hideOnStateChange: true
+    });
+    RoutesService.getRoute(routeId).then(route => {
+      // Load the stops data into the view
+      let [pickups, dropoffs] = BookingService.computeStops(route.trips);
+      if (type === "pickup") $scope.data.stops = pickups;
+      if (type === "dropoff") $scope.data.stops = dropoffs;
+      // Scroll to the selected stop if we have one
+      $ionicScrollDelegate.resize();
+      $ionicLoading.hide();
+    }).catch(error => {
+      // On error close out 
+      $ionicLoading.hide();
+      $ionicPopup.alert({ 
+        title: "Sorry there's been a problem loading the stop information",
+        subTitle: error
+      });
+    });
   }
 ];
