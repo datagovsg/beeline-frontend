423a637f491bf8939e806a4946f7282e8d6d0066
HONGYI LI <lhy@data.gov.sg>
2017-07-26 14:35:11 +0800

    Basic functionality for stoppicker

--- beeline/controllers/RouteDetailController.js
+++ beeline/controllers/RouteDetailController.js
@@ -3,14 +3,28 @@ export default [
   "$state",
   "$stateParams",
   function($scope, $state, $stateParams) {
-    $scope.routeId = $stateParams.routeId;
-    $scope.pickupStop = "no stop picked";
-    $scope.chooseStop = function() {
+    $scope.data = {
+      pickupStop: null,
+      dropoffStop: null
+    }
+    let routeId = $stateParams.routeId;
+    $scope.choosePickup = () => {
       $state.go("tabs.route-stops", {
-        routeId: $stateParams.routeId,
+        routeId: routeId,
         type: "pickup",
-        callback: function(stop) {
-          $scope.pickupStop = stop;
+        stop: $scope.data.pickupStop,
+        callback: (stop) => {
+          $scope.data.pickupStop = stop;
+        }
+      });
+    };
+    $scope.chooseDropoff = () => {
+      $state.go("tabs.route-stops", {
+        routeId: routeId,
+        type: "dropoff",
+        stop: $scope.data.dropoffStop,
+        callback: (stop) => {
+          $scope.data.dropoffStop = stop;
         }
       });
     };
