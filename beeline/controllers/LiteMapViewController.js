4b0d8083f0fab5383d19a7e46bbde0180e12d896
shen_xujing <shen_xujing@ida.gov.sg>
2017-10-02 14:16:40 +0800

    fix indentation and remove unnecessary injected service

--- beeline/controllers/LiteMapViewController.js
+++ beeline/controllers/LiteMapViewController.js
@@ -50,9 +50,6 @@ export default [
       }
     }
 
-
-
-
     LiteRoutesService.fetchLiteRoute(routeLabel).then((response) => {
       var route = response[routeLabel]
       if (route.path) {
@@ -74,65 +71,64 @@ export default [
       SharedVariableService.setStops(liteTripStops)
     })
 
-      MapService.on('ping-trips', (trips) => {
-        $scope.mapObject.pingTrips = trips
-      })
+    MapService.on('ping-trips', (trips) => {
+      $scope.mapObject.pingTrips = trips
+    })
 
-      //fetch driver pings every 4s
-      $scope.timeout = new SafeInterval(pingLoop, 4000, 1000);
+    //fetch driver pings every 4s
+    $scope.timeout = new SafeInterval(pingLoop, 4000, 1000);
 
 
-      MapService.on("killPingLoop", () => {
+    MapService.on("killPingLoop", () => {
+      $scope.timeout.stop();
+    });
+
+    MapService.on("startPingLoop", () => {
+      $scope.timeout.start();
+    });
+
+    //load icons and path earlier by restart timeout on watching trips
+    $scope.$watchCollection('mapObject.pingTrips', (pt) => {
+      if (pt) {
         $scope.timeout.stop();
-      });
-
-      MapService.on("startPingLoop", () => {
         $scope.timeout.start();
-      });
-
-      //load icons and path earlier by restart timeout on watching trips
-      $scope.$watchCollection('mapObject.pingTrips', (pt) => {
-        if (pt) {
-          $scope.timeout.stop();
-          $scope.timeout.start();
-        }
-      });
-
-
-      async function pingLoop() {
-        if (!$scope.mapObject.pingTrips) return;
-
-        $scope.mapObject.statusMessages = $scope.mapObject.statusMessages || []
-        $scope.mapObject.allRecentPings = $scope.mapObject.allRecentPings || []
-
-        $scope.mapObject.statusMessages.length = $scope.mapObject.allRecentPings.length = $scope.mapObject.pingTrips.length
-
-        await Promise.all($scope.mapObject.pingTrips.map((trip, index) => {
-          return TripService.DriverPings(trip.id)
-          .then((info) => {
-            const now = Date.now()
-
-            $scope.mapObject.allRecentPings[index] = {
-              ...info,
-              isRecent: info.pings[0] &&
-                (now - info.pings[0].time.getTime()) < 5 * 60000,
-            }
-
-            $scope.mapObject.statusMessages[index] = _.get(info, 'statuses[0].message', null)
-          })
-        }))
-        //to mark no tracking data if no ping or pings are too old
-        if (_.every($scope.mapObject.allRecentPings,{"isRecent": undefined})) {
-          $scope.hasTrackingData = false;
-        } else {
-          $scope.hasTrackingData = true;
-        }
-        let tripInfo = {
-          'hasTrackingData': $scope.hasTrackingData,
-          'statusMessages': $scope.mapObject.statusMessages.join(' ')
-        }
-        MapService.emit('tripInfo', tripInfo)
       }
+    });
 
+
+    async function pingLoop() {
+      if (!$scope.mapObject.pingTrips) return;
+
+      $scope.mapObject.statusMessages = $scope.mapObject.statusMessages || []
+      $scope.mapObject.allRecentPings = $scope.mapObject.allRecentPings || []
+
+      $scope.mapObject.statusMessages.length = $scope.mapObject.allRecentPings.length = $scope.mapObject.pingTrips.length
+
+      await Promise.all($scope.mapObject.pingTrips.map((trip, index) => {
+        return TripService.DriverPings(trip.id)
+        .then((info) => {
+          const now = Date.now()
+
+          $scope.mapObject.allRecentPings[index] = {
+            ...info,
+            isRecent: info.pings[0] &&
+              (now - info.pings[0].time.getTime()) < 5 * 60000,
+          }
+
+          $scope.mapObject.statusMessages[index] = _.get(info, 'statuses[0].message', null)
+        })
+      }))
+      //to mark no tracking data if no ping or pings are too old
+      if (_.every($scope.mapObject.allRecentPings,{"isRecent": undefined})) {
+        $scope.hasTrackingData = false;
+      } else {
+        $scope.hasTrackingData = true;
+      }
+      let tripInfo = {
+        'hasTrackingData': $scope.hasTrackingData,
+        'statusMessages': $scope.mapObject.statusMessages.join(' ')
+      }
+      MapService.emit('tripInfo', tripInfo)
+    }
   }
 ];
