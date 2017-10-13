d48d010c8f2b65bf148a91cd6d9069c83619e28e
shen_xujing <shen_xujing@ida.gov.sg>
2017-10-13 15:39:14 +0800

    add ga site search documentation link for reference

--- beeline/services/MapService.js
+++ beeline/services/MapService.js
@@ -8,6 +8,8 @@ angular.module('beeline')
 .factory('SearchEventService', (GoogleAnalytics) => {
   const emitter = new EventEmitter()
   emitter.on('search-item', (data) => {
+    // ga site search
+    // https://support.google.com/analytics/answer/1012264?hl=en
     let page =  window.location.hash.substr(1)+'/search?q=' + data
     GoogleAnalytics('send', 'pageview', {
       page: page
