e04ca78089213677c11a21eeb626e8db7e784a49
shen_xujing <shen_xujing@ida.gov.sg>
2017-10-13 15:21:51 +0800

    enable ga site-search, send over search item with  query parameter '?q='

--- beeline/services/MapService.js
+++ beeline/services/MapService.js
@@ -2,3 +2,16 @@ import EventEmitter from 'events'
 
 angular.module('beeline')
 .factory('MapService', () => new EventEmitter())
+
+
+angular.module('beeline')
+.factory('SearchEventService', (GoogleAnalytics) => {
+  const emitter = new EventEmitter()
+  emitter.on('search-item', (data) => {
+    let page =  window.location.hash.substr(1)+'/search?q=' + data
+    GoogleAnalytics('send', 'pageview', {
+      page: page
+    })
+  })
+  return emitter
+})
