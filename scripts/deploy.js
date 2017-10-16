befed9a56577395f121dc31fbddc3a8ea701764f
Alwyn Tan <alwyn.tan@data.gov.sg>
2017-10-16 15:19:12 +0800

    Ensure hot-code push files have appropriate sed scripts

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -55,7 +55,13 @@ inquirer.prompt([
       "-i",
       "app-pages.beeline.sg",
       "app-staging-pages.beeline.sg",
-      ["www/CNAME", "www/chcp.json "]
+      ["www/CNAME"]
+    );
+    shell.sed(
+      "-i",
+      "app.beeline.sg",
+      "app-staging.beeline.sg",
+      ["www/chcp.json "]
     );
     shell.exec("gh-pages -t -d www/");
     console.log(STAGING_COMPLETE_MESSAGE);
