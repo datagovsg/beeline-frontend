a7e3f5a9657b5d1267e51240c60be149cb2a530a
shen_xujing <shen_xujing@ida.gov.sg>
2017-10-31 15:59:04 +0800

    use angular way to define directive

--- beeline/directives/routeItem/animatedRoute.js
+++ beeline/directives/routeItem/animatedRoute.js
@@ -1,7 +1,7 @@
 
 // https://cloudcannon.com/deconstructions/2014/11/15/facebook-content-placeholder-deconstruction.html
-export default function () {
+angular.module('beeline').directive('animatedRoute', function() {
   return {
     template: require('./animatedRoute.html')
   }
-}
+})
