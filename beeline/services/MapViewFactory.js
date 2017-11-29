34250cf22a321eab48614ef2d455b15cc3353f05
Joel Choo <joelchoo@gmail.com>
2017-11-29 10:50:16 +0800

    Changed factory to new style

--- beeline/services/MapViewFactory.js
+++ beeline/services/MapViewFactory.js
@@ -1,51 +1,49 @@
 import { formatTime, formatTimeArray } from "../shared/format"
 import _ from "lodash"
 
-export default [
-  function MapViewFactory() {
-    return {
-      init: function(scope) {
-        scope.mapObject = this.mapObject()
+angular.module("beeline").factory("MapViewFactory", () => {
+  return {
+    init: function($scope) {
+      $scope.mapObject = this.mapObject()
 
-        scope.disp = this.disp()
+      $scope.disp = this.disp()
 
-        scope.closeWindow = function() {
-          scope.disp.popupStop = null
-        }
+      $scope.closeWindow = function() {
+        $scope.disp.popupStop = null
+      }
 
-        scope.applyTapBoard = function(stop) {
-          scope.disp.popupStop = stop
-          scope.$digest()
-        }
+      $scope.applyTapBoard = function(stop) {
+        $scope.disp.popupStop = stop
+        $scope.$digest()
+      }
 
-        scope.formatStopTime = function(input) {
-          if (Array.isArray(input)) {
-            return formatTimeArray(input)
-          }
-          return formatTime(input)
+      $scope.formatStopTime = function(input) {
+        if (Array.isArray(input)) {
+          return formatTimeArray(input)
         }
-      },
-      mapObject: function() {
-        return _.assign(
-          {},
-          {
-            stops: [],
-            routePath: [],
-            alightStop: null,
-            boardStop: null,
-            pingTrips: [],
-            allRecentPings: [],
-            chosenStop: null,
-            statusMessages: [],
-          }
-        )
-      },
-      disp: function() {
-        return {
-          popupStop: null,
-          routeMessage: null,
+        return formatTime(input)
+      }
+    },
+    mapObject: function() {
+      return _.assign(
+        {},
+        {
+          stops: [],
+          routePath: [],
+          alightStop: null,
+          boardStop: null,
+          pingTrips: [],
+          allRecentPings: [],
+          chosenStop: null,
+          statusMessages: [],
         }
-      },
-    }
-  },
-]
+      )
+    },
+    disp: function() {
+      return {
+        popupStop: null,
+        routeMessage: null,
+      }
+    },
+  }
+})
