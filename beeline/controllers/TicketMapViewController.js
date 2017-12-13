bda2cca3a25f49fbf0264a95eb3a086d19f1506f
Joel Choo <joelchoo@gmail.com>
2017-12-13 10:04:55 +0800

    Removed $timeout from TIcketMapViewController

--- beeline/controllers/TicketMapViewController.js
+++ beeline/controllers/TicketMapViewController.js
@@ -5,7 +5,6 @@ export default [
   "SharedVariableService",
   "$stateParams",
   "RoutesService",
-  "$timeout",
   "TripService",
   "TicketService",
   "MapViewFactory",
@@ -14,7 +13,6 @@ export default [
     SharedVariableService,
     $stateParams,
     RoutesService,
-    $timeout,
     TripService,
     TicketService,
     MapViewFactory
