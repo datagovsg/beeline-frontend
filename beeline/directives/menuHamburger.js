ac3bda81018eb44e2c106a4ebd01dc21a7347996
shen_xujing <shen_xujing@ida.gov.sg>
2018-02-07 10:10:16 +0800

    remove gif tutorial and 'menu' for side menu

--- beeline/directives/menuHamburger.js
+++ beeline/directives/menuHamburger.js
@@ -2,12 +2,7 @@ angular.module("beeline").directive("menuHamburger", function() {
   return {
     replace: false,
     template: `
-    <button menu-toggle="left" class="button button-icon">
-      <span class="fa-stack">
-        <span class="fa-stack-1x filetype-text">MENU</span>
-        <i class="fa fa-bars fa-stack-1x"></i>
-      </span>
-    </button>
+    <button menu-toggle="left" class="button button-icon icon ion-navicon"></button>
     `,
   }
 })
