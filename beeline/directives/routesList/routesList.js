1f64c3f66e6b5ac92503c8b864bd6670e41357f2
shen_xujing <shen_xujing@ida.gov.sg>
2018-02-26 11:53:39 +0800

    show "no personal routes" in "tabs.yourRoutes"

--- beeline/directives/routesList/routesList.js
+++ beeline/directives/routesList/routesList.js
@@ -4,6 +4,7 @@ angular.module("beeline").directive("routesList", [
   function() {
     return {
       restrict: "E",
+      replace: "true",
       template: routesListTemplate,
     }
   },
--- beeline/directives/routesList/yourRoutesList.js
+++ beeline/directives/routesList/yourRoutesList.js
@@ -4,6 +4,7 @@ angular.module("beeline").directive("yourRoutesList", [
   function() {
     return {
       restrict: "E",
+      replace: "true",
       template: yourRoutesListTemplate,
     }
   },
