fff638b13b1d332b7d597d694223e322c5d9ac2f
Joel Choo <joelchoo@gmail.com>
2018-06-22 08:06:33 +0800

    kickstartInfo -> crowdstartInfo

--- beeline/directives/crowdstartInfo/crowdstartInfo.js
+++ beeline/directives/crowdstartInfo/crowdstartInfo.js
@@ -3,9 +3,9 @@
 // passHide hides the pass in summary page
 // preorderHide hides the info about asking user to "preorder", takes effect in recap page
 
-angular.module("beeline").directive("kickstartInfo", function() {
+angular.module("beeline").directive("crowdstartInfo", function() {
   return {
-    template: require("./kickstartInfo.html"),
+    template: require("./crowdstartInfo.html"),
     restrict: "E",
     replace: false,
     scope: {
