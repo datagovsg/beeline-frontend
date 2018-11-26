2b5de8fce7216007c857a109803f8be0cd330593
LoneRifle <LoneRifle@users.noreply.github.com>
2018-11-26 17:37:19 +0800

    Reverse geocoding: Allow graceful failures
    * Use last known onemap token in the event of token creation fail
    * Provide bare object when no onemap token, or revgeocode fail

--- beeline/common/OneMapPlaceService.js
+++ beeline/common/OneMapPlaceService.js
@@ -68,36 +68,50 @@ angular.module('common').factory('OneMapPlaceService', [
       var now = new Date().getTime()
 
       /* 10 minutes to expiry */
-      if (lastToken.exp * 1e3 - now > 10 * 60e3) {
-        return lastToken.token
-      } else {
+      if (lastToken.exp * 1e3 - now < 10 * 60e3) {
         var url = 'https://developers.onemap.sg/publicapi/publicsessionid'
 
-        const token = await $http({ url, method: 'GET' })
+        await $http({ url, method: 'GET' })
           .then(response => {
             lastToken.exp = response.data.expiry_timestamp
             lastToken.token = response.data.access_token
             return response.data.access_token
           })
-
-        return token
+          .catch(err => {
+            console.warn('Unable to obtain token, using last known one', err)
+          })
       }
+
+      return lastToken.token
     }
 
     return {
       async reverseGeocode (lat, lng) {
         let token = await getToken()
-        let url = 'https://developers.onemap.sg/publicapi/revgeocode?'
-        let queryString = querystring.stringify({
-          token,
-          location: lat + ',' + lng,
-        })
-        let { data: result } = await $http({
-          method: 'GET',
-          url: url + queryString,
-        })
 
-        return transfromRevGeocodeResults(result.GeocodeInfo[0])
+        const onemapCoords = {
+          LATITUDE: String(lat),
+          LONGITUDE: String(lng),
+        }
+        let geocodeInfo
+        if (token) {
+          let queryString = querystring.stringify({
+            token,
+            location: lat + ',' + lng,
+          })
+          let url = 'https://developers.onemap.sg/publicapi/revgeocode?' + queryString
+          let { data: result } = await $http({ method: 'GET', url })
+            .catch(err => {
+              console.warn('Unable to query onemap for reverse geocode', err)
+              return { data: { GeocodeInfo: [ onemapCoords ] } }
+            })
+          geocodeInfo = result.GeocodeInfo[0]
+        } else {
+          console.warn('Unable to query onemap - no API token available')
+          geocodeInfo = onemapCoords
+        }
+
+        return transfromRevGeocodeResults(geocodeInfo)
       },
 
       async handleQuery (queryText) {
