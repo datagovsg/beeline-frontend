d797e9995f6801e8b5ccaa76d5c9440a73614a03
LoneRifle <LoneRifle@users.noreply.github.com>
2019-01-23 10:28:14 +0800

    De-lint webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,55 +1,55 @@
 /* eslint no-console: 0 */
-const path = require("path")
-const assert = require("assert")
-const InlineEnviromentVariablesPlugin = require("inline-environment-variables-webpack-plugin")
+const path = require('path')
+const assert = require('assert')
+const InlineEnviromentVariablesPlugin = require('inline-environment-variables-webpack-plugin')
 
-const prefix = process.env.BUILD_PREFIX || "www"
+const prefix = process.env.BUILD_PREFIX || 'www'
 
-const INLINED_ENVIRONMENT_VARIABLES = ["BACKEND_URL", "TRACKING_URL"]
+const INLINED_ENVIRONMENT_VARIABLES = ['BACKEND_URL', 'TRACKING_URL']
 
-INLINED_ENVIRONMENT_VARIABLES.forEach(function(key) {
-  assert(process.env[key], "process.env." + key + " must be set")
+INLINED_ENVIRONMENT_VARIABLES.forEach(function (key) {
+  assert(process.env[key], 'process.env.' + key + ' must be set')
 })
 
-module.exports = function(env) {
+module.exports = function (env) {
   env = env || {}
   return [
     // Javascript Config
     {
-      devtool: "source-map",
+      devtool: 'source-map',
       module: {
         rules: [
           // Enable ES6 goodness
           {
             test: /\.js$/,
-            loader: "babel-loader",
+            loader: 'babel-loader',
             exclude: /node_modules/,
           },
           // Allow HTML imports
           {
             test: /\.html$/,
-            loader: "html-loader",
+            loader: 'html-loader',
             options: { attrs: false }, // disable img:src loading
           },
           // Allow JSON imports
           {
             test: /\.json$/,
-            loader: "json-loader",
+            loader: 'json-loader',
           },
           // Fix for libraries that require globals
           {
             test: require.resolve(
-              "multiple-date-picker/dist/multipleDatePicker"
+              'multiple-date-picker/dist/multipleDatePicker'
             ),
-            loader: "imports-loader?moment",
+            loader: 'imports-loader?moment',
           },
         ],
       },
-      entry: ["babel-polyfill", path.resolve("beeline/main.js")],
+      entry: ['babel-polyfill', path.resolve('beeline/main.js')],
       output: {
-        path: path.resolve(prefix, "lib/beeline"),
-        filename: "bundle.js",
-        pathinfo: env.production ? false : true,
+        path: path.resolve(prefix, 'lib/beeline'),
+        filename: 'bundle.js',
+        pathinfo: !env.production,
       },
       plugins: [
         new InlineEnviromentVariablesPlugin(INLINED_ENVIRONMENT_VARIABLES),
